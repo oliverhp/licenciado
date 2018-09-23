c2de8c6a946f94b991559e33acc2e32c80c909df
Oliver Hernandez <heysoyoli@gmail.com>
2018-09-23 02:24:48 -0500

    maybe

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/api/hello', (req, res) => {
 });
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'licenciado05/client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
