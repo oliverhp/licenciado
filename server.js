b7405a5810cb5ddfd57968e56e9b20c507dc75a1
Oliver Hernandez <heysoyoli@gmail.com>
2018-09-23 03:27:27 -0500

    please

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
